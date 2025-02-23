f60745faae17225c944c092fb76b2218200aefd7
Michael Yang <fuhai999@gmail.com>
2025-02-23 11:02:37 +0800

    refactor: use 'children' to replace slot

--- packages/ui/src/components/base/button.svelte
+++ packages/ui/src/components/base/button.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 
     import type { MyHTMLButtonAttributes } from './types';
+    import type { Snippet } from 'svelte';
 
-    const { ...rest }: MyHTMLButtonAttributes = $props();
+    const { children, ...rest }: MyHTMLButtonAttributes & {
+        children?: Snippet;
+    } = $props();
 </script>
 <button type="button" {...rest} class="tf-btn nopan nodrag {rest.class}">
-    <slot></slot>
+    {@render children?.()}
 </button>
