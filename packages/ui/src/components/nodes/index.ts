4cf915157fb047170179e4569039717f20be4e4d
Michael Yang <fuhai999@gmail.com>
2025-02-26 14:28:22 +0800

    feat: add code node

--- packages/ui/src/components/nodes/index.ts
+++ packages/ui/src/components/nodes/index.ts
@@ -2,12 +2,14 @@ import type { NodeTypes } from '@xyflow/svelte';
 import StartNode from './StartNode.svelte';
 import EndNode from './EndNode.svelte';
 import LLMNode from './LLMNode.svelte';
+import CodeNode from './CodeNode.svelte';
 
 /**
  * @description 节点类型（en: node type）
  */
 export const nodeTypes = {
     startNode: StartNode,
+    codeNode: CodeNode,
     llmNode: LLMNode,
-    endNode: EndNode,
-} as any as  NodeTypes;
+    endNode: EndNode
+} as any as NodeTypes;
