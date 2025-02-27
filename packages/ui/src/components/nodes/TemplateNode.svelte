fd1b9bca94ed7b05b410e9dc252b98a6a451097c
Michael Yang <fuhai999@gmail.com>
2025-02-27 17:40:14 +0800

    refactor: optimize node icons

--- packages/ui/src/components/nodes/TemplateNode.svelte
+++ packages/ui/src/components/nodes/TemplateNode.svelte
@@ -19,14 +19,14 @@
 
     const { updateNodeData } = useSvelteFlow();
 
-    $effect(()=>{
-        addParameter(currentNodeId, 'outputDefs',{
+    $effect(() => {
+        addParameter(currentNodeId, 'outputDefs', {
             name: 'output',
             dataType: 'String',
             dataTypeDisabled: true,
-            deleteDisabled: true,
-        })
-    })
+            deleteDisabled: true
+        });
+    });
 
 </script>
 
@@ -36,7 +36,7 @@
     {#snippet icon()}
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path
-                d="M20.7134 7.12811L20.4668 7.69379C20.2864 8.10792 19.7136 8.10792 19.5331 7.69379L19.2866 7.12811C18.8471 6.11947 18.0555 5.31641 17.0677 4.87708L16.308 4.53922C15.8973 4.35653 15.8973 3.75881 16.308 3.57612L17.0252 3.25714C18.0384 2.80651 18.8442 1.97373 19.2761 0.930828L19.5293 0.319534C19.7058 -0.106511 20.2942 -0.106511 20.4706 0.319534L20.7238 0.930828C21.1558 1.97373 21.9616 2.80651 22.9748 3.25714L23.6919 3.57612C24.1027 3.75881 24.1027 4.35653 23.6919 4.53922L22.9323 4.87708C21.9445 5.31641 21.1529 6.11947 20.7134 7.12811ZM9 2C13.0675 2 16.426 5.03562 16.9337 8.96494L19.1842 12.5037C19.3324 12.7367 19.3025 13.0847 18.9593 13.2317L17 14.071V17C17 18.1046 16.1046 19 15 19H13.001L13 22H4L4.00025 18.3061C4.00033 17.1252 3.56351 16.0087 2.7555 15.0011C1.65707 13.6313 1 11.8924 1 10C1 5.58172 4.58172 2 9 2ZM9 4C5.68629 4 3 6.68629 3 10C3 11.3849 3.46818 12.6929 4.31578 13.7499C5.40965 15.114 6.00036 16.6672 6.00025 18.3063L6.00013 20H11.0007L11.0017 17H15V12.7519L16.5497 12.0881L15.0072 9.66262L14.9501 9.22118C14.5665 6.25141 12.0243 4 9 4ZM19.4893 16.9929L21.1535 18.1024C22.32 16.3562 23 14.2576 23 12.0001C23 11.317 22.9378 10.6486 22.8186 10L20.8756 10.5C20.9574 10.9878 21 11.489 21 12.0001C21 13.8471 20.4436 15.5642 19.4893 16.9929Z"></path>
+                d="M2 4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V20C22 20.5523 21.5523 21 21 21H3C2.44772 21 2 20.5523 2 20V4ZM4 5V19H20V5H4ZM7 8H17V11H15V10H13V14H14.5V16H9.5V14H11V10H9V11H7V8Z"></path>
         </svg>
     {/snippet}
 
@@ -62,19 +62,19 @@
                     template: e.target.value
                 }
             })
-        }} value={data.template as string||""}/>
+        }} value={data.template as string||""} />
     </div>
 
 
     <div class="heading">
         <Heading level={3} mt="10px">输出参数</Heading>
-<!--        <Button class="input-btn-more" style="margin-left: auto" onclick={()=>{-->
-<!--            addParameter(currentNodeId, 'outputDefs')-->
-<!--        }}>-->
-<!--            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">-->
-<!--                <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>-->
-<!--            </svg>-->
-<!--        </Button>-->
+        <!--        <Button class="input-btn-more" style="margin-left: auto" onclick={()=>{-->
+        <!--            addParameter(currentNodeId, 'outputDefs')-->
+        <!--        }}>-->
+        <!--            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">-->
+        <!--                <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>-->
+        <!--            </svg>-->
+        <!--        </Button>-->
     </div>
     <OutputDefList />
 
