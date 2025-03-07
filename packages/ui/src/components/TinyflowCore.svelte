b8fed9fab30c29bc3c45cf88339049749635e977
Michael Yang <fuhai999@gmail.com>
2025-03-07 18:00:17 +0800

    feat: add edge setting panel

--- packages/ui/src/components/TinyflowCore.svelte
+++ packages/ui/src/components/TinyflowCore.svelte
@@ -5,7 +5,7 @@
         Controls,
         MiniMap,
         useSvelteFlow,
-        type Node, MarkerType, type Handle
+        type Node, MarkerType, type Handle, Panel
     } from '@xyflow/svelte';
     import '@xyflow/svelte/dist/style.css';
     import '../styles/index.ts';
@@ -15,11 +15,14 @@
     import { genShortId } from './utils/IdGen';
     import { useGetNode } from './utils/useGetNode';
     import { useEnsureParentInNodesBefore } from './utils/useEnsureParentInNodesBefore';
+    import { Textarea } from './base';
 
     const { onInit } = $props();
     const svelteFlow = useSvelteFlow();
 
     onInit(svelteFlow);
+
+    let showEdgePanel = $state(false);
 
 
     const onDragOver = (event: DragEvent) => {
@@ -131,6 +134,18 @@
                 onconnectstart={onconnectstart}
                 onconnect={onconnect}
                 connectionRadius={50}
+                on:edgeclick={() => {
+                    showEdgePanel = true;
+                }}
+                onclick={(e) => {
+                    const el = e.target as HTMLElement;
+                    if (el.classList.contains("svelte-flow__edge-interaction")
+                        || el.classList.contains('panel-content')
+                        || el.closest('.panel-content')){
+                     return
+                    }
+                     showEdgePanel = false;
+                }}
                 defaultEdgeOptions={{
                     // animated: true,
                     // label: 'edge label',
@@ -145,5 +160,40 @@
         <Background />
         <Controls />
         <MiniMap />
+
+        {#if showEdgePanel}
+            <Panel>
+                <div class="panel-content">
+                    <div>边属性设置</div>
+                    <div class="setting-title">边条件设置</div>
+                    <div class="setting-item">
+                        <Textarea
+                            rows={3}
+                            placeholder="请输入边条件"
+                            style="width: 100%"
+                            oninput={(e)=>{
+                            }}
+                        />
+                    </div>
+                </div>
+            </Panel>
+        {/if}
     </SvelteFlow>
 </div>
+
+<style>
+    .panel-content {
+        padding: 10px;
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        width: 200px;
+        border: 1px solid #efefef;
+    }
+
+    .setting-title {
+        margin: 10px 0;
+        font-size: 12px;
+        color: #999;
+    }
+</style>
