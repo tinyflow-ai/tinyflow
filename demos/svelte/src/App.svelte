68d8ee7a5f6342e0ebfc8134d55d1c73ee17f974
Michael Yang <fuhai999@gmail.com>
2025-03-07 15:41:43 +0800

    feat: optimize App.svelte

--- demos/svelte/src/App.svelte
+++ demos/svelte/src/App.svelte
@@ -1,7 +1,6 @@
 <script>
 
     import { Tinyflow } from '@tinyflow-ai/svelte';
-
     // const data = ``
     const data = `{"nodes":[{"id":"2","type":"llmNode","data":{"title":"大模型","description":"处理大模型相关问题","expand":true,"outputDefs":[{"id":"pyiig8ntGWZhVdVz","dataType":"Object","name":"param","children":[{"id":"1","name":"newParam1","dataType":"String"},{"id":"2","name":"newParam2","dataType":"String"}]}]},"position":{"x":550,"y":50},"measured":{"width":322,"height":623},"selected":true},{"id":"3","type":"startNode","data":{"title":"开始节点","description":"开始定义输入参数","expand":true},"position":{"x":150,"y":25},"measured":{"width":306,"height":200},"selected":false},{"id":"4","type":"endNode","data":{"title":"结束节点","description":"结束定义输出参数","expand":true},"position":{"x":950,"y":100},"measured":{"width":306,"height":200},"selected":false}],"edges":[],"viewport":{"x":250,"y":100,"zoom":1}}`;
 
@@ -16,7 +15,7 @@
             <a href="https://github.com/tinyflow-ai/tinyflow" target="_blank">Github</a>
         </div>
     </div>
-    <Tinyflow data={JSON.parse(data)} style={{height:"calc(100vh - 58px)"}} />
+    <Tinyflow data={JSON.parse(data)} style={{height:"calc(100vh - 58px)"}} bind:this={window.tinyflowRef} />
 </main>
 <style lang="less">
   .header {
