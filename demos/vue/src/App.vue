71ea51e446ed7d2764a6d4f58f644aed8b7ce397
Michael Yang <fuhai999@gmail.com>
2025-03-13 17:39:06 +0800

    feat: add demo for vue

---
+++ demos/vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div style="width: 100%; height: calc(100vh - 16px)">
+        <Tinyflow
+            :className="'custom-class'"
+            :style="{ width: '100%', height: '100%' }"
+            :data="initialData"
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Tinyflow } from '@tinyflow-ai/vue';
+import { ref } from 'vue';
+
+const initialData = ref({
+    nodes: [],
+    edges: []
+});
+</script>
