4c7e13d4b229c67c6a3deaa839b2f77f878b584a
Michael Yang <fuhai999@gmail.com>
2025-04-15 12:51:53 +0800

    feat: add edge condition config support

---
+++ packages/ui/src/components/utils/useUpdateEdgeData.ts
@@ -0,0 +1,26 @@
+import { useStore } from '@xyflow/svelte';
+import { get } from 'svelte/store';
+
+export const useUpdateEdgeData = () => {
+    const store = useStore();
+    const updateEdgeData = (id: string, dataUpdate: any, options?: { replace: boolean }) => {
+        const edge = get(store.edgeLookup).get(id);
+        if (!edge) {
+            return;
+        }
+
+        const nextData = typeof dataUpdate === 'function' ? dataUpdate(edge) : dataUpdate;
+        edge.data = options?.replace ? nextData : { ...edge.data, ...nextData };
+        store.edges.update((eds) =>
+            eds.map((e) => {
+                if (e.id === id) {
+                    return edge;
+                }
+                return edge;
+            })
+        );
+    };
+    return {
+        updateEdgeData
+    };
+};
