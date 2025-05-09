ce7576130cf91035a7f7fa2abd56bd90d91f23ae
yangyn <yangyining@deliverik.com>
2025-05-09 21:37:39 +0800

    feat(ui): 修复开始节点参数的reftype不对

--- packages/ui/src/components/nodes/StartNode.svelte
+++ packages/ui/src/components/nodes/StartNode.svelte
@@ -28,7 +28,7 @@
     <div class="heading">
         <Heading level={3}>输入参数</Heading>
         <Button class="input-btn-more" style="margin-left: auto" onclick={()=>{
-            addParameter(currentNodeId)
+            addParameter(currentNodeId, "parameters", {refType: "input"});
         }}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>
