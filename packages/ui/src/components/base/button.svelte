3fdbe07cf61c0d9cf49150aeef0a814850d7b0ba
Michael Yang <fuhai999@gmail.com>
2025-05-17 12:47:48 +0800

    feat: Button 新增 primary 配置

--- packages/ui/src/components/base/button.svelte
+++ packages/ui/src/components/base/button.svelte
@@ -3,10 +3,11 @@
     import type { MyHTMLButtonAttributes } from './types';
     import type { Snippet } from 'svelte';
 
-    const { children, ...rest }: MyHTMLButtonAttributes & {
+    const { children, primary, ...rest }: MyHTMLButtonAttributes & {
         children?: Snippet;
+        primary?: boolean;
     } = $props();
 </script>
-<button type="button" {...rest} class="tf-btn nopan nodrag {rest.class}">
+<button type="button" {...rest} class="tf-btn {primary?'tf-btn-primary':''} nopan nodrag {rest.class}">
     {@render children?.()}
 </button>
