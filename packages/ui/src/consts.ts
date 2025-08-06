6953c0f42e141228bdfb539374574211ffe84158
Michael Yang <fuhai999@gmail.com>
2025-08-06 15:19:44 +0800

    feat: 合并 utils.Consts 到 consts

--- packages/ui/src/consts.ts
+++ packages/ui/src/consts.ts
@@ -1,1 +1,64 @@
 export const componentName = 'tinyflow-component';
+
+export const parameterDataTypes = [
+    {
+        value: 'String',
+        label: 'String'
+    },
+    {
+        value: 'Number',
+        label: 'Number'
+    },
+    {
+        value: 'Boolean',
+        label: 'Boolean'
+    },
+    {
+        value: 'File',
+        label: 'File'
+    },
+    {
+        value: 'Object',
+        label: 'Object'
+    },
+    {
+        value: 'Array',
+        label: 'Array'
+    }
+];
+
+export const parameterRefTypes = [
+    {
+        value: 'ref',
+        label: '引用'
+    },
+    {
+        value: 'fixed',
+        label: '固定值'
+    }
+    // {
+    //     value: 'input',
+    //     label: '输入'
+    // }
+];
+
+export const contentTypes = [
+    { label: '文字', value: 'text' },
+    { label: '图片', value: 'image' },
+    { label: '视频', value: 'video' },
+    { label: '音频', value: 'audio' },
+    { label: '文件', value: 'file' },
+    { label: '其他', value: 'other' }
+];
+
+export const startFormTypes = [
+    { label: '当行输入框', value: 'input' },
+    { label: '多行输入框', value: 'textarea' },
+    { label: '单选', value: 'radio' },
+    { label: '多选', value: 'checkbox' }
+];
+
+export const confirmFormTypes = [
+    { label: '单选', value: 'radio' },
+    { label: '多选', value: 'checkbox' }
+];
