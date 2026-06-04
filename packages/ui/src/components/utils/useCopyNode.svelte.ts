63f09cd04773b81d799019e2aafe6ae7d71d52e3
Michael Yang <fuhai999@gmail.com>
2026-06-04 11:54:24 +0800

    feat(nodes): enhance node copying with deep clone and parameter updates

--- packages/ui/src/components/utils/useCopyNode.svelte.ts
+++ packages/ui/src/components/utils/useCopyNode.svelte.ts
@@ -1,19 +1,28 @@
 import { genShortId } from './IdGen';
 import { store } from '#store/stores.svelte';
+import { deepClone } from '#components/utils/deepClone';
+import type { Parameter } from '#types';
+import type { Node } from '@xyflow/svelte';
 
 export const useCopyNode = () => {
     const copyNode = (id: string) => {
         const node = store.getNode(id);
         if (node) {
-            const newNodeId = genShortId();
             const newNode = {
-                ...node,
-                id: newNodeId,
+                ...deepClone(node),
+                id: genShortId(),
                 position: {
                     x: node.position.x + 50,
                     y: node.position.y + 50
                 }
-            };
+            } as Node;
+
+            if (newNode.data?.parameters) {
+                for (let parameter of newNode.data.parameters as Parameter[]) {
+                    parameter.id = genShortId();
+                    parameter.ref = undefined;
+                }
+            }
 
             store.updateNodes((nodes) => {
                 const newNodes = nodes.map((n) => ({ ...n, selected: false }));
